a6cc9ccfb49dcd0e12c107f98ef1d6a767732c8d
PolarisT <56212320+silence-tang@users.noreply.github.com>
2025-03-23 12:40:00 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 # settings
 TEXT_PROMPT="Audrey Hepburn wearing a tailored blazer, a shirt underneath, straight-cut trousers, and low-heeled shoes."
-IMAGE_PROMPT="/home/tangzichen.tzc/GaussianIP/assets/audrey.png"
-CONFIG_PATH="/home/tangzichen.tzc/GaussianIP/configs/exp.yaml"
-LOG_PATH="/home/tangzichen.tzc/GaussianIP/logs"
+IMAGE_PROMPT="/path/to/GaussianIP/assets/audrey.png"
+CONFIG_PATH="/path/to/GaussianIP/configs/exp.yaml"
+LOG_PATH="/path/to/GaussianIP/logs"
 CUR_TIME=$(date +"%Y%m%d-%H%M%S")
 mkdir -p "$LOG_PATH/$CUR_TIME"
 
@@ -13,13 +13,12 @@ echo "Finished stage 1"
 
 # stage 2: View Consistent Refinement
 echo "Starting stage 2"
-cd /home/tangzichen.tzc/GaussianIP/threestudio/models/guidance
+cd /path/to/GaussianIP/threestudio/models/guidance
 CUDA_VISIBLE_DEVICES=0 python refine.py --config_path "${CONFIG_PATH}" --log_path "${LOG_PATH}" --cur_time "${CUR_TIME}" --pil_image_path "${IMAGE_PROMPT}" --prompt "$TEXT_PROMPT"
-# CUDA_VISIBLE_DEVICES=0 python refine.py  --config_path /home/tangzichen.tzc/GaussianIP/configs/exp.yaml  --root_path /home/tangzichen.tzc/GaussianIP --pil_image_path /home/tangzichen.tzc/GaussianIP/assets/audrey.png --prompt Audrey Hepburn wearing a tailored blazer, a shirt underneath, straight-cut trousers, and low-heeled shoes.
 echo "Finished stage 2"
 
 # stage 3: 3D Reconstruction
 echo "Starting stage 3"
-cd /home/tangzichen.tzc/GaussianIP
+cd /path/to/GaussianIP
 CUDA_VISIBLE_DEVICES=0 python launch.py --train system.cur_time="${CUR_TIME}"
 echo "Finished stage 3"
