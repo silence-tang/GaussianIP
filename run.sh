0f450cd901bb1bdce0b3146f82c0693f96421306
PolarisT <56212320+silence-tang@users.noreply.github.com>
2025-03-30 15:08:00 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,17 +8,17 @@ mkdir -p "$LOG_PATH/$CUR_TIME"
 
 # stage 1: AHDS
 echo "Starting stage 1"
-CUDA_VISIBLE_DEVICES=0 python launch.py --train system.prompt_processor.prompt="$TEXT_PROMPT" system.guidance.pil_image_faceid_path="${IMAGE_PROMPT}" system.root_path="${LOG_PATH}" system.cur_time="${CUR_TIME}"
+CUDA_VISIBLE_DEVICES=0 python launch.py --cur_time "$CUR_TIME" --train system.prompt_processor.prompt="$TEXT_PROMPT" system.guidance.pil_image_faceid_path="${IMAGE_PROMPT}" system.log_path="${LOG_PATH}" system.cur_time="${CUR_TIME}"
 echo "Finished stage 1"
 
 # stage 2: View Consistent Refinement
 echo "Starting stage 2"
-cd /path/to/GaussianIP/threestudio/models/guidance
+cd /home/tangzichen.tzc/GaussianIP/threestudio/models/guidance
 CUDA_VISIBLE_DEVICES=0 python refine.py --config_path "${CONFIG_PATH}" --log_path "${LOG_PATH}" --cur_time "${CUR_TIME}" --pil_image_path "${IMAGE_PROMPT}" --prompt "$TEXT_PROMPT"
 echo "Finished stage 2"
 
 # stage 3: 3D Reconstruction
 echo "Starting stage 3"
-cd /path/to/GaussianIP
-CUDA_VISIBLE_DEVICES=0 python launch.py --train system.cur_time="${CUR_TIME}"
+cd /home/tangzichen.tzc/GaussianIP
+CUDA_VISIBLE_DEVICES=0 python launch.py --cur_time "$CUR_TIME" --train system.cur_time="${CUR_TIME}"
 echo "Finished stage 3"
