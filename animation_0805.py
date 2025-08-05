da3e192dba3740b26edfdbc185eb90987e702186
PolarisT <56212320+silence-tang@users.noreply.github.com>
2025-08-05 22:24:56 +0800

    Update and rename animation_0501.py to animation_0805.py

--- animation_0805.py
+++ animation_0805.py
@@ -8,15 +8,22 @@ from gs_renderer import Renderer, MiniCam
 def animate(dataset, pipe):
     # gaussian model
     renderer = Renderer(sh_degree=0, white_background=False)
+    renderer.gaussians.load_ply_ori('/root/autodl-tmp/GaussianIP/data/humans/last.ply')
 
     scene = Avatar(dataset, renderer.gaussians)
-    scene.gaussians.load_ply_ori('/root/autodl-tmp/GaussianIP/data/humans/last.ply')
+    # NOTE: 这边似乎用load_ply_new会导致飘出来的身体部分
+    # NOTE: 原始3dgs, humangaussian anime, anime-gs都用的这个
 
     bg_color = [1, 1, 1] if dataset.white_background else [0, 0, 0]
     background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
 
-    motion_path = '/root/autodl-tmp/GaussianIP/data/SFU_SMPLX/0007/0007_Crawling001_stageii.npz'
+    # motion_path = '/root/autodl-tmp/GaussianIP/data/SFU_SMPLX/0007/0007_Crawling001_stageii.npz'
+    # motion_path = '/root/autodl-tmp/GaussianIP/data/SFU_SMPLX/0007/0007_Cartwheel001_stageii.npz'
+    # motion_path = '/root/autodl-tmp/GaussianIP/data/SFU_SMPLX/0005/0005_SideSkip001_stageii.npz'
+    motion_path = '/root/autodl-tmp/GaussianIP/data/SFU_SMPLX/0008/0008_Yoga001_stageii.npz'
+    
     scene.animate(motion_path, pipe=pipe, bg=background)
+    # scene.render_canonical(nframes=180, pose_type=None, pipe=pipe, bg=background)
 
 if __name__ == "__main__":
     # Set up command line argument parser
