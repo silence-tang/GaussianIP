609f9da994d4c944b72da2d9586558d1f4a14f84
PolarisT <56212320+silence-tang@users.noreply.github.com>
2025-08-05 22:24:05 +0800

    Update __init__.py

--- avatar/__init__.py
+++ avatar/__init__.py
@@ -11,9 +11,17 @@
 import os
 from gs_renderer import GaussianModel
 import torch
-from modules.smplx_custom import SMPLX
+from custom_smplx.smplx_custom import SMPLX
 import numpy as np
 from utils.general import create_video
+
+from human_body_prior.tools.model_loader import load_model
+from human_body_prior.models.vposer_model import VPoser
+
+from human_body_prior.body_model.body_model import BodyModel
+from body_visualizer.tools.vis_tools import render_smpl_params
+from body_visualizer.tools.vis_tools import imagearray2file
+# from body_visualizer.tools.vis_tools import show_image
 
 # import smplx
 
@@ -53,6 +61,7 @@ class Avatar:
         """
         self.device = 'cuda'
         self.gaussians = gaussians
+
         # 加载smpl模型
         # self.smpl = SMPL(SMPL_PATH).to(self.device)
         # self.smplx = smplx.SMPLX(SMPLX_PATH).to(self.device)
@@ -65,9 +74,13 @@ class Avatar:
             ext='npz',
             flat_hand_mean=True,
         ).to(self.device)
+        self.ori_center = None
 
         self.get_A_verts()
+        # self.get_vitruvian_verts()
 
+        self.vposer, self.ps = load_model('/root/autodl-tmp/GaussianIP/utils/human_body_prior/V02_05', model_code=VPoser, remove_words_in_model_weights='vp_model.', disable_grad=True)
+        self.vposer = self.vposer.to('cuda')
 
     @torch.no_grad()
     def get_vitruvian_verts(self):
@@ -85,7 +98,7 @@ class Avatar:
         betas = torch.zeros(10)
         self.betas = torch.tensor(betas, dtype=torch.float32, device=self.device)
 
-        smpl_output = self.smpl(body_pose=vitruvian_pose, betas=self.betas[None], disable_posedirs=False)
+        smpl_output = self.smplx(body_pose=vitruvian_pose, betas=self.betas[None], disable_posedirs=False)
         vitruvian_verts = smpl_output.vertices[0]
         self.T_t2vitruvian = smpl_output.T[0].detach()
         self.inv_T_t2vitruvian = torch.inverse(self.T_t2vitruvian)
@@ -105,8 +118,8 @@ class Avatar:
         a_body_pose[1, 2] = -0.1
         a_body_pose[15, 2] = -0.7853982
         a_body_pose[16, 2] = 0.7853982
-        a_body_pose[19, 0] = 1.0
-        a_body_pose[20, 0] = 1.0
+        # a_body_pose[19, 0] = 1.0
+        # a_body_pose[20, 0] = 1.0
 
         # betas = None
         # expression=None
@@ -122,16 +135,40 @@ class Avatar:
             expression=self.expression,
             return_verts=True
         )
-        
+
         a_verts = smplx_output.vertices[0]
+        a_verts_numpy = a_verts.detach().cpu().numpy()
+
+        # rescale and recenter
+        if self.ori_center is None:
+            vmin = a_verts_numpy.min(0)
+            vmax = a_verts_numpy.max(0)
+            self.ori_center = torch.tensor((vmax + vmin) / 2, device='cuda') # [-9.793043e-05, -0.43814677, -0.005535446]
+            self.ori_scale = 0.6 / np.max(vmax - vmin) # 0.3500760954126092
+
+        # NOTE: 先把高斯变换到smplx位置？
+        self.unscale_points(self.gaussians._xyz, -10)
+        self.gaussians._xyz = self.gaussians._xyz / self.ori_scale + self.ori_center
+        
         self.T_t2a = smplx_output.T[0].detach()
         self.inv_T_t2a = torch.inverse(self.T_t2a)
         
         self.canonical_offsets = smplx_output.shape_offsets + smplx_output.pose_offsets
         self.canonical_offsets = self.canonical_offsets[0]
         self.a_verts = a_verts.detach()
+        # self.a_verts = (self.a_verts - self.ori_center) * self.ori_scale
+        # self.scale_verts(-10)
         return a_verts.detach()
 
+    def scale_verts(self, delta):
+        # self.points3D[:, :3] *= 1.1 ** (-delta)
+        if self.a_verts is not None:
+            self.a_verts *= 1.1 ** (-delta)
+    
+    def unscale_points(self, points, delta):
+        # self.points3D[:, :3] *= 1.1 ** (-delta)
+        if points is not None:
+            points /= 1.1 ** (-delta)
 
     @torch.no_grad()
     def render_canonical(self, nframes=180, pose_type=None, pipe=None, bg=None):
@@ -144,10 +181,13 @@ class Avatar:
         # NOTE: 还需探究fovx, fovy等参数对渲染结果的影响
         camera_params = get_rotating_camera(
             img_size=1024,
-            fov=1.222,
-            dist=2.0,
-            znear=0.1,
-            zfar=1000.0,
+            fov=0.87266, # animation.py是50 deg, 训练是70 deg
+            # fov=1.222, # 70 deg
+            dist=2.5,
+            # znear=0.1,  # 训练
+            # zfar=1000.0, # 训练
+            znear=0.01, # animation.py
+            zfar=100.0, # animation.py
             device='cuda',
             nframes=nframes,
             angle_limit=2 * torch.pi,
@@ -164,8 +204,35 @@ class Avatar:
             full_proj_transform = cam_p["full_proj_transform"]
 
             # 将每帧的相机参数封装为MiniCam对象(viewpoint_cam)
-            viewpoint_cam = MiniCam(width, height, fovy, fovx, 0.1, 1000.0, world_view_transform, full_proj_transform)
+
+            # world_view_transform(animation.py):
+            # tensor([[ 1.0000, -0.0000, -0.0000,  0.0000],
+            #         [ 0.0000, -1.0000, -0.0000,  0.0000],
+            #         [ 0.0000, -0.0000, -1.0000,  0.0000],
+            #         [-0.0000, -0.0000,  2.5000,  1.0000]], device='cuda:0')
+            
+            # full_proj_transform(animation.py):
+            # tensor([[ 2.1445,  0.0000,  0.0000,  0.0000],
+            #         [ 0.0000, -2.1445,  0.0000,  0.0000],
+            #         [ 0.0000,  0.0000, -1.0001, -1.0000],
+            #         [ 0.0000,  0.0000,  2.4902,  2.5000]], device='cuda:0')
+
+            # world_view_transform(__init__.py):
+            # tensor([[ 1.0000,  0.0000,  0.0000,  0.0000],
+            #         [ 0.0000, -1.0000,  0.0000,  0.0000],
+            #         [ 0.0000,  0.0000, -1.0000,  0.0000],
+            #         [ 0.0000,  0.0000,  2.5000,  1.0000]], device='cuda:0')
+            
+            # full_proj_transform(__init__.py):
+            # tensor([[ 2.1445,  0.0000,  0.0000,  0.0000],
+            #         [ 0.0000, -2.1445,  0.0000,  0.0000],
+            #         [ 0.0000,  0.0000, -1.0001, -1.0000],
+            #         [ 0.0000,  0.0000,  2.4902,  2.5000]], device='cuda:0')
+
+            viewpoint_cam = MiniCam(width, height, fovy, fovx, 0.1, 100.0, world_view_transform, full_proj_transform)
             # 把加载的高斯原封不动丢进去执行渲染
+            # TODO: 改成在smplx表面初始化高斯然后渲染, 并查看初始化的高斯与smplx表面的最近邻vs加载的高斯与smplx表面的最近邻
+            # TODO: 将smplx顶点进行与animation.py中同样的变换, 并查看加载的高斯与smplx表面的最近邻距离是否小于smplx顶点不变换的距离
             render_pkg = render(viewpoint_cam, self.gaussians, pipe, bg)
             image = render_pkg["render"]
             # 存图
@@ -184,31 +251,77 @@ class Avatar:
         motions = np.load(motion_path)
         motion_len = motions['root_orient'].shape[0]
         start_idx = 0
-        end_idx = motion_len
+        end_idx = motion_len // 4
         skip = 4
 
+        # root_orient
         root_orient = torch.tensor(motions['root_orient'][start_idx:end_idx:skip], dtype=torch.float32, device='cuda')
+        ro_value = torch.tensor([0, 0, 0], device='cuda')
+        root_orient = ro_value.repeat(root_orient.shape[0], 1)
+
+        # transl
         transl = torch.tensor(motions['trans'][start_idx:end_idx:skip], dtype=torch.float32, device='cuda')
+        transl_value = torch.tensor([0.0, 0.0, 0.0], device='cuda')
+        transl = transl_value.repeat(root_orient.shape[0], 1)
+
+        # betas
+        betas = torch.zeros([root_orient.shape[0], 3], dtype=torch.float32, device='cuda')
+
+        # pose
         pose_body = torch.tensor(motions['pose_body'][start_idx:end_idx:skip], dtype=torch.float32, device='cuda')
+
+        # body_pose = torch.zeros((21, 3), dtype=torch.float32, device='cuda')
+        # body_pose[0, 1] = 0.2
+        # body_pose[0, 2] = 0.1
+        # body_pose[1, 1] = -0.2
+        # body_pose[1, 2] = -0.1
+        # body_pose[15, 2] = -0.7853982
+        # body_pose[16, 2] = 0.7853982
+        # body_pose[19, 0] = 1.0
+        # body_pose[20, 0] = 1.0
+        # body_pose = body_pose.reshape(63)
+        # pose_body = body_pose.repeat(root_orient.shape[0], 1)
+
+        # load body model
+        # bm = BodyModel(bm_fname='/root/autodl-tmp/smplx/SMPLX_NEUTRAL.npz').to('cuda')
+
+        # # 随机采样pose
+        # pose_body_all = []
+        # for i in range(pose_body.shape[0]):
+        #     latent_pose_code = torch.from_numpy(np.random.randn(1, 32).astype(np.float32)).to('cuda')
+        #     pose_body = self.vposer.decode(latent_pose_code)['pose_body'].contiguous().view(-1, 63)
+        #     # images = render_smpl_params(bm, {'pose_body':pose_body}).reshape(1,1,1,400,400,3)
+        #     # imagearray2file(images, f'/root/autodl-tmp/GaussianIP/output/anim/test_pose_body_{i}.png')
+        #     pose_body_all.append(pose_body.reshape(63))
+        #     # 将pose写入text中, 方便根据图找到索引再找到对应的pose参数
+        
+        # with open('/root/autodl-tmp/GaussianIP/human_body_prior/tutorials/output/anim/pose.txt', 'w') as file:
+        #     for pose_body in pose_body_all:
+        #         file.write(f"{pose_body.tolist()}\n")
+
+        # pose_body = torch.stack(pose_body_all, dim=0).to('cuda')
 
         # NOTE: 定义渲染视频所需的相机参数, 需确认是否与gaussianip对齐
         camera_params = get_rotating_camera(
             img_size=1024,
-            fov=0.873, # 50 deg
-            dist=4.0,
-            znear=0.1,
-            zfar=100.0,
+            fov=0.87266, # animation.py是50 deg, 训练是70 deg
+            # fov=1.222,
+            dist=2.5,
+            # znear=0.1,  # 训练
+            # zfar=1000.0, # 训练
+            znear=0.01, # animation.py
+            zfar=100.0, # animation.py
             device='cuda',
-            nframes=1,
+            nframes= len(root_orient),
             angle_limit=2 * torch.pi,
         )
 
         ext_tfs = None
-        manual_trans = torch.tensor((np.array([0, -1, 0])), dtype=torch.float32, device='cuda')
-        manual_rot = np.array([-90.0, 0, 0]) / 180 * np.pi
-        manual_rotmat = torch.tensor((transformations.euler_matrix(*manual_rot)[:3, :3]),dtype=torch.float32, device='cuda')
-        manual_scale = torch.tensor((1.0), dtype=torch.float32, device='cuda')
-        ext_tfs = (manual_trans.unsqueeze(0), manual_rotmat.unsqueeze(0), manual_scale.unsqueeze(0))
+        # manual_trans = torch.tensor((np.array([0, -1, 0])), dtype=torch.float32, device='cuda') # 不然人会飘在天上
+        # manual_rot = np.array([-90.0, 0, 0]) / 180 * np.pi
+        # manual_rotmat = torch.tensor((transformations.euler_matrix(*manual_rot)[:3, :3]),dtype=torch.float32, device='cuda')
+        # manual_scale = torch.tensor((1.0), dtype=torch.float32, device='cuda')
+        # ext_tfs = (manual_trans.unsqueeze(0), manual_rotmat.unsqueeze(0), manual_scale.unsqueeze(0))
         
         for idx in tqdm(range(pose_body.shape[0]), desc="Animation"):
             print(idx)
@@ -226,6 +339,8 @@ class Avatar:
                 betas=self.betas,
                 expression=self.expression,
                 transl=transl[idx-1],
+                # NOTE: 如果设置为None, 则人物位置偏下
+                # transl=None, 
                 ext_tfs=ext_tfs,
             )
 
@@ -236,7 +351,16 @@ class Avatar:
             rotations = output['rotq']
             active_sh_degree = output['active_sh_degree']
 
-            # NOTE: 每帧都用相同的相机参数进行渲染
+            # # debug: 用cano属性去渲染
+            # feats = output['shs']
+            # means3D = output['xyz_canon']
+            # opacity = output['opacity']
+            # scales = output['scales_canon']
+            # rotations = output['rotq_canon']
+            # active_sh_degree = output['active_sh_degree']
+
+            # NOTE: 每帧都用相同的相机参数进行渲染(azi=0, 正面视角)
+            # cam_p = camera_params[idx]
             cam_p = camera_params[0]
 
             # 解析出当前使用的相机参数
@@ -284,8 +408,8 @@ class Avatar:
         gs_scales_canon = gs_scales.clone()
         gs_rotmat = quaternion_to_matrix(gs_rotq)
 
-        if hasattr(self, 'global_orient') and global_orient is None:
-            global_orient = rotation_6d_to_axis_angle(self.global_orient[dataset_idx].reshape(-1, 6)).reshape(3)
+        # if hasattr(self, 'global_orient') and global_orient is None:
+        #     global_orient = rotation_6d_to_axis_angle(self.global_orient[dataset_idx].reshape(-1, 6)).reshape(3)
 
         if hasattr(self, 'body_pose') and body_pose is None:
             body_pose = rotation_6d_to_axis_angle(self.body_pose[dataset_idx].reshape(-1, 6)).reshape(23 * 3)
@@ -296,8 +420,8 @@ class Avatar:
         if hasattr(self, 'expression') and expression is None:
             expression = self.expression
 
-        if hasattr(self, 'transl') and transl is None:
-            transl = self.transl[dataset_idx]
+        # if hasattr(self, 'transl') and transl is None:
+        #     transl = self.transl[dataset_idx]
 
         # a-pose -> t-pose -> tgt-pose
         # remove & reapply the blendshape
@@ -322,15 +446,30 @@ class Avatar:
         T_t2pose = smplx_output.T[0]
         T_a2t = self.inv_T_t2a.clone()
         T_a2t[..., :3, 3] = T_a2t[..., :3, 3] + self.canonical_offsets - curr_offsets
-        T_a2pose = T_t2pose @ T_a2t
+        T_a2pose = T_t2pose @ T_a2t # NOTE: 注意是标准姿态下的a2pose变换，而不是recenter&rescale后的a2pose变换
+        
+        # T_t2pose = smplx_output.T[0]
+        # T_vitruvian2t = self.inv_T_t2vitruvian.clone()
+        # T_vitruvian2t[..., :3, 3] = T_vitruvian2t[..., :3, 3] + self.canonical_offsets - curr_offsets
+        # T_vitruvian2pose = T_t2pose @ T_vitruvian2t
 
+        # TODO: 改成在smplx表面初始化高斯然后渲染, 并查看初始化的高斯与smplx表面的最近邻vs加载的高斯与smplx表面的最近邻
+        # TODO: 将smplx顶点进行与animation.py中同样的变换, 并查看加载的高斯与smplx表面的最近邻距离是否小于smplx顶点不变换的距离
         lbs_gau = smplx_lbs_diffuse_gau_topk(
             lbs_weights=self.smplx.lbs_weights,         # 固定的smplx顶点的lbs权重
-            verts_transform=T_a2pose.unsqueeze(0),      # 当前姿态的a2pose变换
+            verts_transform=T_a2pose.unsqueeze(0),      # 所有smplx顶点的a2pose变换
             points=gs_xyz.unsqueeze(0),                 # 标准姿态下的高斯位置
             template_points=self.a_verts.unsqueeze(0),  # 标准姿态下的mesh顶点位置
-            K=16
+            K=6
         )
+        
+        # lbs_gau = smplx_lbs_diffuse_gau_topk(
+        #     lbs_weights=self.smplx.lbs_weights,         # 固定的smplx顶点的lbs权重
+        #     verts_transform=T_vitruvian2pose.unsqueeze(0),      # 所有smplx顶点的a2pose变换
+        #     points=gs_xyz.unsqueeze(0),                 # 标准姿态下的高斯位置
+        #     template_points=self.vitruvian_verts.unsqueeze(0),  # 标准姿态下的mesh顶点位置
+        #     K=6
+        # )
 
         lbs_gau = lbs_gau.squeeze(0)
 
@@ -361,7 +500,7 @@ class Avatar:
         self.gaussians.normals = torch.zeros_like(gs_xyz)
         self.gaussians.normals[:, 2] = 1.0
 
-        deformed_gs_shs = gs_shs.clone()
+        deformed_gs_shs = gs_shs
 
         return {
             'xyz': deformed_xyz,  # deform之后的高斯位置
@@ -464,9 +603,19 @@ def smplx_lbs_diffuse_gau_topk(
     addition_info=None
 ):
     '''ref: https://github.com/JanaldoChen/Anim-NeRF
-    Args:
     '''
     bz, np, _ = points.shape
+
+    # points[0].cpu().numpy().min(0)
+    # array([-0.5872435 , -0.8470391 , -0.16731435], dtype=float32)
+    # points[0].cpu().numpy().max(0)
+    # array([0.59194416, 0.82413304, 0.17705269], dtype=float32)
+
+    # template_points.cpu().numpy()[0].min(0)
+    # array([-0.5637325 , -0.7781227 , -0.13287593], dtype=float32)
+    # template_points.cpu().numpy()[0].max(0)
+    # array([0.5637325 , 0.7781227 , 0.13287593], dtype=float32)
+
     with torch.no_grad():
         results = knn_points(points, template_points, K=K)
         dists, idxs = results.dists, results.idx
@@ -475,7 +624,7 @@ def smplx_lbs_diffuse_gau_topk(
     NNs = idxs
     weight_std = 0.1
     weight_std2 = 2. * weight_std ** 2
-    conf_threshold = 0.0
+    conf_threshold = 0.5
 
     # 取出各高斯点的最近邻, [1, gau_num, k, joint_num]
     gau_NNs_lbs_weights = lbs_weights[NNs]  
@@ -501,4 +650,3 @@ def smplx_lbs_diffuse_gau_topk(
     gau_transform = torch.sum(gau_NNs_weights.unsqueeze(-1).unsqueeze(-1) * gau_NNs_transform, dim=2)
     
     return gau_transform
-
